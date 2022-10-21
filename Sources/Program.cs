e4b63d94d3ea08a99919f4c488e58a990c7640e2
Heinrich Schaefer <heinrich.schaefer92@gmail.com>
2022-10-22 00:32:49 +0200

    Feat: User input via console.

--- Sources/Program.cs
+++ Sources/Program.cs
@@ -1,3 +1,4 @@
-﻿int integer = 100; // change me
+﻿Console.WriteLine("Please enter an integer between 1 and 3999");
+var integer = Int32.Parse(Console.ReadLine());
 var romanNumber = Converter.ConvertToRoman(integer);
 Console.WriteLine(integer + " = " + romanNumber);
